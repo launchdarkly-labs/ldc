29f071f1c903b233a6250029542dbe59d57d561e
Andrew S. Brown <andrew@launchdarkly.com>
2018-12-19 20:15:13 -0800

    Add back support for config file with multiple accounts

--- cmd/util.go
+++ cmd/util.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/abiosoft/ishell"
+import (
+	"strings"
+
+	"github.com/abiosoft/ishell"
+)
 
 func confirmDelete(c *ishell.Context, name string, expectedValue string) bool {
 	if !interactive {
@@ -9,4 +13,21 @@ func confirmDelete(c *ishell.Context, name string, expectedValue string) bool {
 	c.Printf("Re-enter the %s '%s' to delete: ", name, expectedValue)
 	value := c.ReadLine()
 	return value == expectedValue
+}
+
+func withPrefix(keys []string, prefix string) []string {
+	var completions []string
+	for _, key := range keys {
+		if strings.HasPrefix(key, prefix) {
+			completions = append(completions, key)
+		}
+	}
+	return completions
+}
+
+func toPrefix(args []string) string {
+	if len(args) == 0 {
+		return ""
+	}
+	return args[0]
 }
