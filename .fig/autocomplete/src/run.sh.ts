4d0708b37a6cc549bedb68f0a802218a168e5a96
Jaz White <jwhite@launchdarkly.com>
2021-12-14 14:49:52 -0500

    add project commands

--- .fig/autocomplete/src/run.sh.ts
+++ .fig/autocomplete/src/run.sh.ts
@@ -1,3 +1,6 @@
+// Brand colors
+const LD_BLUE_HEX = '405BFF';
+
 const configNameGenerator: Fig.Generator = {
   //TODO this should optionally use a config specified with --config
   script: "jq 'keys' ~/.config/ldc.json",
@@ -12,7 +15,82 @@ const configNameGenerator: Fig.Generator = {
       };
     });
   }
+};
+
+const projectGenerator: Fig.Generator = {
+  script(context) {
+    let cmd = './run.sh projects list';
+    const config = getOptionFromContext(context, configOpt);
+    if (config) {
+      cmd += ` --config ${config}`;
+    }
+    const configFile = getOptionFromContext(context, configFileOpt);
+    if (configFile) {
+      cmd += ` --config-file ${configFile}`;
+    }
+
+    return cmd;
+  },
+  postProcess(out) {
+    return out.split("\n").reduce((acc, line) => {
+      const match = line.match(
+        /^\| (?<key>[^\s]+) +\| (?<name>.+\b)\s+\|$/
+      );
+  
+      if (match !== null) {
+        const { key, name } = match.groups;
+        acc.push({
+          name: key,
+          insertValue: key,
+          description: name,
+          icon: `fig://template?color=${LD_BLUE_HEX}&badge=P`
+        });
+      }
+
+      return acc;
+    }, []);
+  },
+};
+
+const getOptionFromContext = (context, option: Fig.Option) => {
+  const index = getOptionIndexFromContext(context, option);
+  const value = index > -1 ? context[index+1] : '';
+
+  return value;
 }
+
+const getOptionIndexFromContext = (context, option: Fig.Option) => {
+  for (const name of option.name) {
+    const idx = context.indexOf(name);
+    if (idx > -1) {
+      return idx;
+    }
+  }
+
+  return -1;
+}
+
+const configOpt: Fig.Option = {
+  name: ["--config"],
+  description: "Configuration to use",
+  args: [
+    {
+      name: "config name",
+      generators: configNameGenerator,
+    },
+  ],
+};
+
+const configFileOpt: Fig.Option = {
+  name: ["--config-file"],
+  description: "Configuration file to use",
+  args: [
+    {
+      name: "config file path",
+      template: "filepaths",
+    },
+  ],
+};
 
 const completionSpec: Fig.Spec = {
   name: "ldc",
@@ -105,7 +183,53 @@ const completionSpec: Fig.Spec = {
           ],
         },
       ],
-    }
+    },
+    {
+      name: "projects",
+      description: "Create, list, view, and delete projects",
+      subcommands: [
+        {
+          name: "list",
+          description: "List projects",
+        },
+        {
+          name: "show",
+          description: "View project details",
+          args: [
+            {
+              name: "project key",
+              generators: projectGenerator,
+              debounce: true,
+            },
+          ],
+        },
+        {
+          name: ["create", "new"],
+          description: "Create a project",
+          args: [
+            {
+              name: "project key",
+            },
+          ],
+        },
+        {
+          name: ["delete", "remove"],
+          description: "Delete a project",
+          args: [
+            {
+              name: "project key",
+              isDangerous: true,
+              generators: projectGenerator,
+              debounce: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  options: [
+    configOpt,
+    configFileOpt,
   ],
 };
 export default completionSpec;
